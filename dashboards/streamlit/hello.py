8b78fe7afbcc2034c194c534b68d2002955b5e85
Andy Lassiter <andrewl@wustl.edu>
2024-05-29 13:45:00 -0500

    Testing tags on another branch

--- dashboards/streamlit/hello.py
+++ dashboards/streamlit/hello.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
 st.title("Streamlit Demo App")
-st.write(f"Hello!")
+st.write(f"Hello World!")
+st.write(f"This is a demo app to show how to deploy a Streamlit app with XNAT data.")
